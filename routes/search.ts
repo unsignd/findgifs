cd9e3e5a60748286a211438d03769dc856bffc75
unsignd <unsigndid@gmail.com>
2024-10-02 01:19:01 -0700

    change to multi api keys

--- routes/search.ts
+++ routes/search.ts
@@ -3,9 +3,11 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
+let apiKeyIndex = 0;
+
 router.get('/search', async (req: Request, res: Response) => {
   const query = req.query.query as string;
-  const apiKey = process.env.KEY_GIPHY;
+  const apiKeyList = process.env.KEY_GIPHY;
 
   if (query === undefined || query.length > 50) {
     res.status(400).send({
@@ -15,48 +17,60 @@ router.get('/search', async (req: Request, res: Response) => {
     return;
   }
 
-  if (!apiKey) {
+  if (!apiKeyList) {
     res.status(500).send({
       error: 'API key not provided.',
     });
     return;
   }
 
-  try {
-    let response = await axios.get('https://api.giphy.com/v1/gifs/search', {
-      params: {
-        q: query,
-        limit: 10,
-        api_key: apiKey,
-      },
-    });
-
-    let gifs = response.data.data.map((gif: any) => {
-      let url = gif.images.downsized_medium.url;
-      let width = gif.images.downsized_medium.width;
-      let height = gif.images.downsized_medium.height;
-
-      return {
-        url:
-          url.slice(0, 8) +
-          'media.' +
-          url.slice(url.indexOf('giphy.com'), url.indexOf('?cid=')),
-        size: {
-          width,
-          height,
+  for (let i = 0; i < apiKeyList.length; i++) {
+    try {
+      let response = await axios.get('https://api.giphy.com/v1/gifs/search', {
+        params: {
+          q: query,
+          limit: 10,
+          api_key: apiKeyList.split(', ')[apiKeyIndex],
         },
-      };
-    });
+      });
 
-    res.send({
-      data: gifs,
-    });
-  } catch (error) {
-    console.error('Fail to search GIFs: ', error);
+      let gifs = response.data.data.map((gif: any) => {
+        let url = gif.images.downsized_medium.url;
+        let width = gif.images.downsized_medium.width;
+        let height = gif.images.downsized_medium.height;
 
-    res.status(500).send({
-      error: 'Fail to search GIFs.',
-    });
+        return {
+          url:
+            url.slice(0, 8) +
+            'media.' +
+            url.slice(url.indexOf('giphy.com'), url.indexOf('?cid=')),
+          size: {
+            width,
+            height,
+          },
+        };
+      });
+
+      res.send({
+        data: gifs,
+      });
+
+      break;
+    } catch (error) {
+      if (i === apiKeyList.length - 1) {
+        console.error('Fail to search GIFs: ', error);
+
+        res.status(500).send({
+          error: 'Fail to search GIFs.',
+        });
+      }
+    }
+
+    if (apiKeyIndex < apiKeyList.split(', ').length - 1) {
+      apiKeyIndex += 1;
+    } else {
+      apiKeyIndex = 0;
+    }
   }
 });
 
