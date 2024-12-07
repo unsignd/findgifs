8e9d8b62179cc4799b378704572947fc3bdda181
unsignd <unsigndid@gmail.com>
2024-12-06 22:15:17 -0800

    please, please be smart

--- src/units/card.tsx
+++ src/units/card.tsx
@@ -1,18 +1,6 @@
 import { useEffect } from 'react';
 import styled from 'styled-components';
 
-const AdWrapper = styled.div`
-  width: 100% !important;
-  height: calc(100% - 60px) !important;
-  min-height: 300px;
-`;
-
-const Ad = styled.ins`
-  width: 100%;
-  height: calc(100% - 60px);
-  display: block;
-`;
-
 export function CardUnit() {
   useEffect(() => {
     try {
@@ -22,26 +10,18 @@ export function CardUnit() {
   });
 
   return (
-    <AdWrapper>
-      {/* <Ad
-        className="adsbygoogle"
-        data-ad-format="fluid"
-        data-ad-layout-key="+1m+s0-k-t+3t"
-        data-ad-client={process.env.REACT_APP_ADSENSE_CID}
-        data-ad-slot="4390596796"
-      ></Ad> */}
-      <ins
-        className="adsbygoogle"
-        style={{
-          display: 'inline-block',
-          width: '100%',
-          height: 'calc(100% - 60px)',
-        }}
-        data-ad-client={process.env.REACT_APP_ADSENSE_CID}
-        data-ad-slot="4390596796"
-        data-full-width-responsive="true"
-        data-ad-format="auto"
-      ></ins>
-    </AdWrapper>
+    <ins
+      className="adsbygoogle"
+      style={{
+        display: 'inline-block',
+        width: '100%',
+        height: 'calc(100% - 60px)'
+        minHeight: 300,
+      }}
+      data-ad-client={process.env.REACT_APP_ADSENSE_CID}
+      data-ad-slot="4390596796"
+      data-full-width-responsive="true"
+      data-ad-format="rectangle"
+    ></ins>
   );
 }
