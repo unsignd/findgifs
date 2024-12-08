7cb3f048599792b33376f24cf6d8676ffcb86720
unsignd <unsigndid@gmail.com>
2024-12-07 22:03:25 -0800

    does css work?

--- src/units/card.tsx
+++ src/units/card.tsx
@@ -1,4 +1,14 @@
 import { useEffect } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: calc(100% - 60px);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export function CardUnit() {
   useEffect(() => {
@@ -14,7 +24,7 @@ export function CardUnit() {
       style={{
         display: 'inline-block',
         width: '100%',
-        height: 'calc(100% - 60px)',
+        height: '100%',
         minHeight: 300,
       }}
       data-ad-client={process.env.REACT_APP_ADSENSE_CID}
