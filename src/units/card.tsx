79882e4f1c7a58f4a8ddc7ecff5eb35c8306f66b
unsignd <unsigndid@gmail.com>
2024-12-08 22:41:06 -0800

    asdf

--- src/units/card.tsx
+++ src/units/card.tsx
@@ -1,13 +1,4 @@
 import { useEffect } from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  width: 100%;
-
-  position: relative;
-
-  bottom: 0;
-`;
 
 export function CardUnit() {
   useEffect(() => {
@@ -18,21 +9,18 @@ export function CardUnit() {
   });
 
   return (
-    <Container>
-      <ins
-        className="adsbygoogle"
-        style={{
-          // position: 'relative',
-          display: 'inline-block',
-          width: '100%',
-          height: '100%',
-          minHeight: 300,
-          // top: '50%',
-          // left: '50%',
-        }}
-        data-ad-client={process.env.REACT_APP_ADSENSE_CID}
-        data-ad-slot="4390596796"
-      ></ins>
-    </Container>
+    <ins
+      className="adsbygoogle"
+      style={{
+        display: 'inline-block',
+        width: '100%',
+        height: 'calc(100% - 60px)',
+        minHeight: 300,
+      }}
+      data-ad-client={process.env.REACT_APP_ADSENSE_CID}
+      data-ad-slot="4390596796"
+      data-full-width-responsive="true"
+      data-ad-format="rectangle"
+    />
   );
 }
