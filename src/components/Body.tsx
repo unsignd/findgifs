2c03b866bb77de63053edf40268559c81c5af223
unsignd <unsigndid@gmail.com>
2024-12-09 00:20:56 -0800

    mbmb

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -217,7 +217,7 @@ export function Body() {
                         ).length !== 0
                     )
                     .map((gif, index) =>
-                      index % 10 === (time * Math.floor(index / 10)) % 10
+                      index % 10 === (time * (Math.floor(index / 10) + 1)) % 10
                         ? [gif, 0]
                         : [gif]
                     )
