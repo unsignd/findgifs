2b3dc9f51918192cb759d0c01870b06cd05bfb93
unsignd <unsigndid@gmail.com>
2024-12-15 18:42:05 -0800

    add banner ad unit

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,13 @@
 import { Banner } from '../components/Banner';
 import { Body } from '../components/Body';
+import { BannerUnit } from '../units/banner';
 
 export function Landing() {
   return (
-    <div>
+    <>
       <Banner />
+      <BannerUnit />
       <Body />
-    </div>
+    </>
   );
 }
--- src/units/banner.tsx
+++ src/units/banner.tsx
@@ -1,19 +1,26 @@
 import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+
+  padding-top: 40px;
+`;
+
 const Ad = styled.ins<{
   $isLoaded: boolean;
 }>`
-  width: 100%;
-  height: calc(100% - 60px);
-  min-height: 300px;
+  width: calc(100% - 80px);
 
-  display: inline-block;
+  display: block;
 
   visibility: ${(props) => (props.$isLoaded ? 'visible' : 'hidden')};
 `;
 
-export function CardUnit() {
+export function BannerUnit() {
   const unitRef = useRef(null);
 
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
@@ -38,14 +45,16 @@ export function CardUnit() {
   }, [unitRef]);
 
   return (
-    <Ad
-      $isLoaded={isLoaded}
-      ref={unitRef}
-      className="adsbygoogle"
-      data-ad-client={process.env.REACT_APP_ADSENSE_CID}
-      data-ad-slot="4390596796"
-      data-full-width-responsive="true"
-      data-ad-format="rectangle"
-    />
+    <Wrapper>
+      <Ad
+        $isLoaded={isLoaded}
+        ref={unitRef}
+        className="adsbygoogle"
+        data-ad-client={process.env.REACT_APP_ADSENSE_CID}
+        data-ad-slot="2834752792"
+        data-full-width-responsive="horizontal"
+        data-ad-format="rectangle"
+      />
+    </Wrapper>
   );
 }
