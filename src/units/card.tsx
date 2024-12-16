da4a07149df8a9b6dc5893fcbc16d41a1dae746e
unsignd <unsigndid@gmail.com>
2024-12-15 20:28:22 -0800

    test

--- src/units/card.tsx
+++ src/units/card.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+`;
+
 const Ad = styled.ins<{
   $isLoaded: boolean;
 }>`
   width: 100%;
-  height: calc(100% - 60px);
+
+  max-height: calc(100% - 60px);
   min-height: 300px;
 
   display: inline-block;
@@ -38,14 +47,22 @@ export function CardUnit() {
   }, [unitRef]);
 
   return (
-    <Ad
-      $isLoaded={isLoaded}
-      ref={unitRef}
-      className="adsbygoogle"
-      data-ad-client={process.env.REACT_APP_ADSENSE_CID}
-      data-ad-slot="4390596796"
-      data-full-width-responsive="true"
-      data-ad-format="rectangle"
-    />
+    <Wrapper>
+      <Ad
+        $isLoaded={isLoaded}
+        ref={unitRef}
+        className="adsbygoogle"
+        data-ad-client={process.env.REACT_APP_ADSENSE_CID}
+        data-ad-slot="4390596796"
+        data-full-width-responsive="true"
+        data-ad-format="rectangle"
+      />
+      <div
+        style={{
+          width: '100%',
+          height: 60,
+        }}
+      />
+    </Wrapper>
   );
 }
